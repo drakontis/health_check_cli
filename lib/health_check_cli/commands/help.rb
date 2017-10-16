41bde0c36f51caa8ad6e2f221c9412a11bbc99ea
Drakontis Panagiotis <drakontis.panagiotis@gmail.com>
2017-10-16 23:08:59 +0300

    Adds help messages

--- lib/health_check_cli/commands/help.rb
+++ lib/health_check_cli/commands/help.rb
@@ -3,6 +3,8 @@ module HealthCheckCli
     class Help < Command
       def execute(opts = {})
         puts 'help             Shows this help message
+init             (Re)Initialises the application with a new endpoint.
+show             Shows the current application that is under health check.
 status           Shows the status of the application and prints the response time
 exit             Exits the application.'
       end
